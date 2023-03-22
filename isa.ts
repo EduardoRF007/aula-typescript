8e4fc52c5f543eaa08184df9b30c837b76e365e3
Eduardo Reis <eduardo.reis@gdc.local>
2023-03-22 14:55:27 -0300

    first commit

---
+++ isa.ts
@@ -0,0 +1,9 @@
+const nome = "Isa"
+
+// 2023 - 19
+const anoCorrente: number = 2023
+const idade: number = 19
+const resultado = anoCorrente - idade
+// 2023 - 2007
+const anoNascimento: number = 2012
+const resultado1 = anoCorrente - anoNascimento
